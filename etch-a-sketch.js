cc003718b40d81721fd51b271b0d16d4e97e4559
andrewtech95 <133996700+andrewtech95@users.noreply.github.com>
2023-08-02 12:08:26 +0200

    Add random color feature

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,10 @@
-const DEFAULT_SIZE = 16; 
+const DEFAULT_SIZE = 30; 
 const btn = document.querySelector('button');
 const container = document.querySelector('.container');
 
 btn.addEventListener('click', () => { 
-  let size = prompt('Enter number of squares per side (max 100)');
-  if (size === null) return;
+  let size = +prompt('Enter number of squares per side (max 100)');
+  if (!size) return;
   else if (size > 100 || size < 0) alert('Invalid entry. Try again');
   else {
     container.innerHTML = '';
@@ -16,9 +16,16 @@ function setupGrid(size) {
   for (let i=0; i<size**2; i++) {
     const gridElement = document.createElement('div');
     gridElement.style.cssText = `width: ${800/size}px; height: ${800/size}px;`;
-    gridElement.addEventListener('mousedown', () => gridElement.classList.add('color'));
+    gridElement.addEventListener('mouseover', changeColor);
     container.appendChild(gridElement);
   }
+}
+
+function changeColor(e) {
+  const randomR = Math.floor(Math.random()*256);
+  const randomG = Math.floor(Math.random()*256);
+  const randomB = Math.floor(Math.random()*256);
+  e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
 }
 
 setupGrid(DEFAULT_SIZE);
